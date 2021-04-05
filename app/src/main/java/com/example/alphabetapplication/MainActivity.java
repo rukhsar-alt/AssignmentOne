cbe0f20808a154654f35725ca5a3c64b792cfb31
mcsf19a020 <mcsf19a020@pucit.edu.pk>
2021-04-06 02:46:42 +0500

    Resource Added

--- app/src/main/java/com/example/alphabetapplication/MainActivity.java
+++ app/src/main/java/com/example/alphabetapplication/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.alphabetapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gridView=findViewById(R.id.gridview);
+        ArrayList<String> alpha=new ArrayList<String>(26);
+        int j=65;
+        for (int i=0;i<26;i++)
+        {
+            alpha.add(Character.toString((char)j));
+            j++;
+        }
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,alpha);
+        gridView.setAdapter(arrayAdapter);
+
     }
 }
